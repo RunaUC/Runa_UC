a96cba9fafbcef0983c6d710a4834a16ebae618c
RunaUC <126734833+RunaUC@users.noreply.github.com>
2023-03-22 12:07:46 +1000

    Create SRP301.R
    on the way to downloading IMOS

---
+++ SRP301.R
@@ -0,0 +1,41 @@
+### SRP301 ### 
+## written by Runa Uchikune (1135219) 
+
+# Packages ----------------------------------------------------------------
+
+library(XML)
+library(tidyverse)
+library(ncdf4)
+library(terra)
+library(raster)
+
+# folders -----------------------------------------------------------------
+
+
+# Function ----------------------------------------------------------------
+
+getIMOSsst <- function(yr) {
+  url <- paste0("http://rs-data1-mel.csiro.au/imos-srs/sst/ghrsst/L3S-1d/dn/", yr) # The URL for the year of data
+  pageContent <- readLines(url) # Read the web page
+  Links <- getHTMLLinks(pageContent) %>% # Get the lines of stuff
+    .[grepl(".nc", .)] %>% # Isolate the names of the linked netCDF files
+    paste0(url, "/", .) # Paste the path at the front 
+  download_imos_file <- function(l) { # An internal function to download link l
+    file_name <- basename(l) # Strip out everything before the file name
+    out_name <- paste0(output_folder, "/", file_name) # File name including path of download folder
+    if(!file.exists(out_name)) { # Download ONLY files that aren't already downloaded
+      download.file(l, out_name) # Download the file and write the file to the download folder
+    }
+  } # End of internal function
+  walk(Links, download_imos_file) # Welcome to purrr! For each link, download the associated file and write it to the output folder (i.e., execute the internal function)
+}
+
+# Do the work -------------------------------------------------------------
+
+years <- 1992:2020 # Set up some years of data to grab
+output_folder <- "/Volumes/RunaSRPDisk/IMOS_Data" 
+if(!dir.exists(output_folder)) {dir.create(output_folder)}# Write down a path to save to (NOT GitHub!)
+walk(years, getIMOSsst) # For each year, run the function
+
+getOption("timeout") # allow R session downloads that take longer than 60sec 
+options(timeout = 10000)
