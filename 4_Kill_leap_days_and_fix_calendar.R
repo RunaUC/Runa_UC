3c635e752043b286ef2dbcc6de5a33b4c5a5613d
RunaUC <126734833+RunaUC@users.noreply.github.com>
2023-04-05 14:48:02 +1000

    homework
    homework for 05/04/2023

--- 4_Kill_leap_days_and_fix_calendar.R
+++ 4_Kill_leap_days_and_fix_calendar.R
@@ -14,13 +14,18 @@
   # We need an input_folder to be set****
   # We need an output_folder to write to (and it needs to be created, if it does not exist)****
 
+input_folder <- "/Volumes/Runa_Disk/allIMOS" 
+output_folder <- "/Volumes/Runa_Disk/annual_merged_IMOS" # create a new folder
+if(!dir.exists(output_folder)) {dir.create(output_folder, recursive=TRUE)}
 
 # Identify the file with all IMOS data together and remove leap days, then fix calendar -----------------
 
-  # xxxxxxx <- dir(yyyyyyy, full.names = TRUE) # A list of the annual netCDFs in the input_folder****
-  cdo_code <- paste0("cdo -L -setcalendar,365_day -delete,month=2,day=29 ", # First delete leap days, then set the calendar
-                     xxxxxxx, # The files with all the IMOS data together***
+  fixeddays_files <- dir(input_folder, full.names = TRUE) # A list of the annual netCDFs in the input_folder****
+  cdo_code <- paste0("cdo -L -setcalendar,365_day -delete,month=2,day=29 ", # First delete leap days, then set the calendar 
+                     fixeddays_files, # The files with all the IMOS data together***
                      " ", # A space
                      paste0(output_folder, "/365_day_IMOS_data.nc") # An output file path and name
                      )
   system(cdo_code) # Send the code chunk to the terminal
+
+  
