bd6456cb706e06494a0a7c2f98bd1f630286ef8d
RunaUC <126734833+RunaUC@users.noreply.github.com>
2023-05-23 10:56:08 +1000

    challenges
    8: got a hint from David and it worked!
    9: tried to read the code but got errors, need a hint?

--- 8_average_cmip6.R
+++ 8_average_cmip6.R
@@ -2,6 +2,11 @@
  # Written by Runa
   # May 2023
 
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+library(raster)
+
 # Folders ----------------------------------------------------------------
 # Scenario1
 input_file <- "/Volumes/Runa_Disk/CMIP6_ensemble/tos_Oday_ensemble_ssp119_r1i1p1f1_rg_19930101-21001231.nc"
@@ -52,7 +57,8 @@ system(cdo_code)
 
 # Since you repeat the code on Lines 22-26 several times, it could more easily be used as a function (see Lines 58-67, below), then get a list of files and deploy the function for each file (see Lines 69-70, below):
 
-output_folder <-  "/Volumes/Runa_Disk/CMIP6_ensemble_means" # Specify an output folder
+input_folder <- "/Volumes/Runa_Disk/CMIP6_ensemble"
+output_folder <- "/Volumes/Runa_Disk/CMIP6_ensemble_mean" # Specify an output folder
 if(!dir.exists(output_folder)) {dir.create(output_folder, recursive=TRUE)} # Make the folder, if it doesn't exist
 
 do_ens_mean <- function(input_file) {
--- 9_CMIP6_anomalies.R
+++ 9_CMIP6_anomalies.R
@@ -17,13 +17,13 @@
 
   observations_folder <- "/Volumes/Runa_Disk/CMIP6_ensemble" # The input folder from script 8
   means_folder <- "/Volumes/Runa_Disk/CMIP6_ensemble_means" # The output folder from script 8
-  output_folder <- "" #*** Make a new folder to put the results in
+  output_folder <- "/Volumes/Runa_Disk/CMIP6_ensemble_" #*** Make a new folder to put the results in
     if(!dir.exists(output_folder)) {dir.create(output_folder, recursive=TRUE)} # If the output folder doesn't exist, make it
 
 
 # Anomalies (subtract mean from observations)  --------
   
-  do_CMIP_anomalies <- funciton(obs) {
+ do_CMIP_anomalies <- function(obs) {
     f_name <- basename(obs) # Just the input file name, which is still in standard format—we can use this to make other file names
     mn <- f_name %>% 
       gsub("_ensemble_", "_ensembleMean_", .) %>%  # Like we did last time
@@ -31,9 +31,15 @@
     output_file <- f_name %>% 
       gsub("_ensemble_", "_anomalies_", .) %>%  # Like we did last time
       paste0(output_folder, "/", .) # Add in the correct path
-    cdo_code <- paste0("cdo ") # the format to subtract the mean from the observations is: cdo[fill in the stuff here] sub obs mn optput_file
+    cdo_code <- paste0("cdo sub ",  
+                       obs, 
+                       " ", 
+                       mn, 
+                       " ", 
+                       output_file) # the format to subtract the mean from the observations is: cdo[fill in the stuff here] sub obs mn output_file
       system(cdo_code)
     }
   
-  files <- dir(observations_folder, pattern = "ssp", full.names = TRUE)
-  walk(files, do_CMIP_anomalies)
+files <- dir(observations_folder, pattern = "ssp", full.names = TRUE)
+walk(files, do_CMIP_anomalies)
+  
