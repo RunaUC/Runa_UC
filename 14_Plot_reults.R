e1bdc4f04d8a545dca121515f274ffb1d6b1f8c7
RunaUC <126734833+RunaUC@users.noreply.github.com>
2023-06-02 10:51:17 +1000

    Update 14_Plot_reults.R

--- 14_Plot_reults.R
+++ 14_Plot_reults.R
@@ -1,34 +1,43 @@
+# plot the results 
+ # June 2023
+
+# packages ----------------------------------------------------------------------
 library(raster)
 library(tmap)
 library(tidyverse)
 library(colorRamps)
 
-input_folder <- "/Users/davidschoeman/Dropbox/Documents/Student_Documents/Ongoing_Students/SRP/Runa_Uchikune/Code_n_data/BC_Cum_Int"
+# folders and files -------------------------------------------------------------
+input_folder <- "/Volumes/Runa_Disk/BC_Cum_Int"
 
 files <- dir(input_folder, full.names = TRUE)
 
-recent <- 2001:2020
-mid_cent <- 2041:2060
-end_cent <- 2081:2100
-all <- 1993:2100
-idx <- 1:length(all)
+# start creating maps -----------------------------------------------------------
+# based on SST transition for 20 years in different sequence of years 
+recent <- 2001:2020 # recent changes (2001 to 2020)
+mid_cent <- 2041:2060 # mid-century changes (2041 to 2060)
+end_cent <- 2081:2100 # end-century changes (2081 to 2100)
+all <- 1993:2100 # entire time range between 1993 to 2100
+idx <- 1:length(all) 
 
 recent_i <- idx[all %in% recent]
 mid_i <- idx[all %in% mid_cent]
 end_i <- idx[all %in% end_cent]
+# these lines create new vectors "recent_i, mid_i, and end_i" that contains the indices corresponding to each year in the "all" sequence. In other words, it extracts the indices of the years that belong to the recent/mid_cent/end_cent and assigned to the vectors. 
 
+# start analyzing SST changes in the different time period -----------------------
 get_rel_MHW <- function(f) {
   r <- stack(f)
     out <- stack(
-      calc(subset(r, recent_i), sum),
+      calc(subset(r, recent_i), sum), # calculate the sum of values in the raster stack "r" for three specific time periods # these time periods are all subsets of stack "r". 
       calc(subset(r, mid_i), sum),
       calc(subset(r, end_i), sum)
       )
     out1 <- stack(
-      out[[2]]/out[[1]],
+      out[[2]]/out[[1]], 
       out[[3]]/out[[1]]
-      )
-    periods <- c("2041—2060", "2081—2100")
+      ) # new raster stack "out1" by dividing the second/third layers of the "out" stack by the first layer. # first layer: recent_i, second layer: mid_i, third layer: end_i # since we do not need the actual temperature it self, but need "How much the temperature might rise compare to the recent (historical SST) period of time. Therefore, these lines divided raster layer 2 and 3 by 1; so that we are able to see the relative values of the future SST under different scenarios relative to the recent SST. 
+    periods <- c("2041—2060", "2081—2100") # defines the name of layers. 
     l_names <- basename(f) %>% 
       str_split("_", simplify = TRUE) %>% 
       .[1, 4] %>% 
@@ -41,7 +50,7 @@ results <- map(files, get_rel_MHW) %>%
   stack()
 
 
-# Plot results
+# Plot results by using tmap function ---------------------------------------------
 brks <- c(0, 1, 2, 3, 5, 7.5, 10, 15, 20, 25, 30, 35, 40)
 tm_shape(results) +
   tm_raster(palette = matlab.like(255), 
